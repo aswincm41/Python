04b64d4d05bd40b4be3b373bb0fd0376cf5ab193
aswincm41 <156645944+aswincm41@users.noreply.github.com>
2024-01-18 11:04:41 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,47 @@
+for x in range(6):
+    for i in range(x):
+     print("*", end="     ")
+    print()
+
+  
+# num = 1
+# rows = 4
+
+# for i in range(1, rows + 1):
+#     for j in range(i):
+#         print(num, end="   ")
+#         num += 1
+#     print()
+
+# n = 4 
+# for i in range(n):
+#     char = ord('A')  # ASCII value of 'A'
+#     for j in range(i + 1):
+#         print(chr(char), end="   ")
+#         char += 1
+#     print()
+
+# n = 4
+# char = ord('A')
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(chr(char), end="   ")
+#         char += 1
+#     print()
+
+# n = 4  
+# num = 2
+
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(num, end=" ")
+#         num += 2
+#     print()
+
+n = 4
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end=" ")
+    for k in range(2 * i + 1):
+        print("#", end=" ")
+    print()
