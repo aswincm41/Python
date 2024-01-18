2a475b678b6bc8f1e0ce7e2b815d36de4deeb54a
aswincm41 <156645944+aswincm41@users.noreply.github.com>
2024-01-18 12:04:22 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,81 @@
+####Append##3
+# x=[1,2,3,"apple"]
+# x.append("orange")
+# x.append(6)
+# print(x)
+
+
+###Insert
+# x=[1,2,3,"apple"]
+# x.insert(4,"manu")
+# x.remove("manu")
+# x.pop(2)
+# print(x)
+
+# a=[1,2,3,4,]
+# a.append(5)
+# a.append("aswin")
+# a.append(7)
+# print(a)
+
+# Append
+
+
+# b=["apple","orange","banana"]
+# b.insert(2,"cherry")
+# print(b)
+
+# insert
+
+# c=[1,2,3,4,"orange"]
+# c.remove(3)
+# c.remove("orange")
+# print(c)
+
+# # remove
+
+# d=["user","client","manager"]
+# d.pop(0)
+# print(d)
+
+# # pop
+
+
+# squared_numbers = [x**2 for x in range(5)]
+# print(squared_numbers)
+
+
+# numbers = list(range(1, 11))
+# result = [x**2 if x % 2 == 0 else x**3 for x in numbers]
+# print(result)
+
+# List Comprehension
+
+# tuple1=(1,2,3)
+# tuple2=('a','b','c')
+# combined_tuple=tuple1+tuple2
+# print(combined_tuple)
+
+# combine tuples
+# x=(1,2,3)
+# y=('a','b','c')
+# z=x+y
+# print(z)
+# combine tuples
+
+
+# a={1,3,4,67}
+# a.add(9)
+# print(a)
+
+# a = {1, 3, 4, 65,5}
+# a.add(9)
+# print(a)
+# a = {1, 3, 4, 65, 5}
+# a.add(9)
+# print(a)
+
+set1={1,2,3}
+set2={3,4,5,6}
+x=set1-set2
+print(x)
